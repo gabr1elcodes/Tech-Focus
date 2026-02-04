f642b715dcda4d5bb506217ce194e30591a65612
Gabriel Oliveira <gabrieloliveira30p@gmail.com>
2026-02-04 17:47:01 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://backend-techfocus.onrender.com',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
